e51d412a3c30b7dd42dec436b9c283658a21becf
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-25 15:23:36 +0100

    basic work complete

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue ({
+    el: '#container',
+    data : {
+        message: 'Hello World!'
+    }
+});
