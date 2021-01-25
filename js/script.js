909d6eba2afde8d695a18725c564e506be385687
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-25 17:08:57 +0100

    creating dinamic nav bar

--- js/script.js
+++ js/script.js
@@ -2,19 +2,33 @@
 
 // Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo. Poi aggiungete all'immagine una classe sempre presa dal data.
 
+
+var arrayMenu = ["Chi siamo", "Da dove veniamo", "Dove andiamo", "Quando si mangia?"];
+
+
+
+
+
+
+
+
+
+// Vue.js
+
 var nav = new Vue ({
-    el: 'nav',
+    el: '#container_vue',
     data: {
         message: 'Logo del sito',
-        bcgBlue: "blue"
-    }
-});
-
-var header = new Vue ({
-    el: 'header',
-    data: {
         img: "https://cdn.pixabay.com/photo/2020/04/30/15/34/code-5113374__340.jpg",
         alt: "Development",
-        border: "border"
+        border: "border",
+        white: "color_white",
+        none: "none"
+    },
+
+    methods: {
+        clickMenu: function(){
+            (this.none == false) ? this.none = true : this.none = false;
+        }
     }
 });
